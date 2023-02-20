4276106fce969380b2cc0181572df09ace9abc38
pauloeufrasio <henriquepaulo0606@gmail.com>
2023-02-20 20:31:40 -0300

    TesteFor

--- src/TesteFor.java
+++ src/TesteFor.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class TesteFor {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int fator = 0;
+        int multiplicação=0;
+        System.out.println("Digite o Valor do Fator: ");
+        fator = sc.nextInt();
 
-        for (int i = 0; i <= 10; i++){
-            System.out.println(i);
+        for (int i = 1; i <= 10; i++){
+            multiplicação=fator*i;
+            System.out.println(+fator+" X "+ i +" = "+multiplicação);
+            System.out.println();
         }
     }
 }
