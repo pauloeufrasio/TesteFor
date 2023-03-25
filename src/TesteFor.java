26240e559cc1b30fd3da5903b71ddee5cbeca74c
paulo <henriquepaulo0606@gmail.com>
2023-03-25 10:55:39 -0300

    Criando tabuada de multiplicação

--- src/TesteFor.java
+++ src/TesteFor.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 public class TesteFor {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // Inicializando as variavel
         int fator = 0;
-        int multiplicação=0;
+        int multiplicacao=0;
+        // Recebendo entrada de dados do usuario
         System.out.print("Digite a tabuada desejada: ");
         fator = sc.nextInt();
-
+        // Criando laço for para (variavel I igual a 1) (variavel I for menor igual a 10) ele incrementa mais um
         for (int i = 1; i <= 10; i++){
-            multiplicação=fator*i;
-            System.out.println(+fator+" X "+ i +" = "+multiplicação);
-            System.out.println();
+        // Base de calculo: multiplicação é igual o fator multiplicado por variavel (I)
+            multiplicacao=fator*i;
+            System.out.println(+fator+" X "+ i +" = "+multiplicacao);
+
         }
+        sc.close();
     }
 }
